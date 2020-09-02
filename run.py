21ac25e8e965aea6606ab067b053b2c54d370152
Arthur Machado <50958424+arthur-machado@users.noreply.github.com>
2020-09-02 17:35:56 -0300

    Adicionado porcentagem de disco

--- run.py
+++ run.py
@@ -4,28 +4,30 @@ import socket
 
 def get_cpu_usage():
 
-    return psutil.cpu_percent(interval=1)
+    return str(psutil.cpu_percent(interval=1))
         
 
 def get_mem_usage():
     
     mem = psutil.virtual_memory()
 
-    return mem.percent
+    return str(mem.percent)
 
 def get_IP():
-    print (socket.getfqdn(socket.gethostname()))
+    hostname = socket.gethostname()
+    ip = socket.gethostbyname(hostname)
 
-    
-    
-     
-# =========================================================
+    return ip
 
-print("=" * 39)
-print("||      CPU      ||      MEMORY       ||")
-print("=" * 39)
-get_IP()
+def get_disk():
+    return str(psutil.disk_usage('/').percent)
+
+
+print("=" * 59)
+print("||      CPU      ||      MEMORY       ||      DISK       ||")
+print("=" * 59)
+
 while True:
-    print("||     "+str(get_cpu_usage())+"%      ||      "+str(get_mem_usage())+"%       ||")
-    print("-"*39)
+    print("||     "+ get_cpu_usage()+"%      ||      "+get_mem_usage()+"%       ||      "+get_disk()+"%      ||")
+    print("-"*59)
     time.sleep(1)
