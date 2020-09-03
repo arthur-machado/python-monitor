5ff5b80fb6691c6a64ea1c208796cd646928f208
Arthur Machado <50958424+arthur-machado@users.noreply.github.com>
2020-09-03 13:38:25 -0300

    Adicionado comentários

--- run.py
+++ run.py
@@ -2,32 +2,36 @@ import psutil
 import time
 #import socket
 
+# retorna a porcentagem de uso da CPU no intervalo de 1 segundo
 def get_cpu_usage():
 
     return str(psutil.cpu_percent(interval=1))
-        
 
+# retorna a porcentagem de uso da memória
 def get_mem_usage():
     
     mem = psutil.virtual_memory()
 
     return str(mem.percent)
 
-#está retornando o endereço local (127.0.0.1) 
+# está retornando o endereço local (127.0.0.1) 
 #def get_IP():
     #hostname = socket.gethostname()
     #ip = socket.gethostbyname(hostname)
 
     #return ip
 
+# quanto espçao de disco está ocupando na raíz (para sistemas Linux) 
 def get_disk():
+    
     return str(psutil.disk_usage('/').percent)
 
-
+# desenha o cabeçalho do monitor
 print("=" * 59)
 print("||      CPU      ||      MEMORY       ||      DISK       ||")
 print("=" * 59)
 
+#imprime as informações enquanto o programa rodar
 while True:
     print("||     "+ get_cpu_usage()+"%      ||      "+get_mem_usage()+"%       ||      "+get_disk()+"%      ||")
     print("-"*59)
