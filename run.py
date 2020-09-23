180b280d166d701a3f3c9705dd3b46b7260f17ea
Arthur Machado <arthurmachado7@gmail.com>
2020-09-23 15:03:42 -0300

    Adicionado cores à CPU e RAM

--- run.py
+++ run.py
@@ -11,15 +11,30 @@ def get_cpu_usage():
     if use < 45:
         return u"\u001b[32;1m" + use_str
     
-    elif use >= 45 and use < 75:
+    elif use >= 45 and use <= 83.9:
         return u"\u001b[33m" + use_str
+
+    elif use >= 84:
+        return u"\033[1;31;40m" + use_str
 
 # retorna a porcentagem de uso da memória
 def get_mem_usage():
     
-    mem = psutil.virtual_memory()
+    mem_total = psutil.virtual_memory().total / (1024 ** 3)
+    mem_used = psutil.virtual_memory().used / (1024 ** 3)
+    percent = round((mem_used * 100 / mem_total), 1)
+    mem_str = str(percent)
 
-    return str(mem.percent)
+    if percent < 45.0:
+        return u"\u001b[32;1m" + mem_str
+    
+    elif percent >= 45 and percent <= 83.9:
+        return u"\u001b[33m" + mem_str
+
+    elif percent >= 84:
+        return u"\033[1;31;40m" + mem_str
+
+
 
 # está retornando o endereço local (127.0.0.1) 
 #def get_IP():
@@ -45,15 +60,16 @@ print("Disk usage: " + get_disk()+"%\n")
 
 # desenha o cabeçalho do monitor
 print(u"\u001b[37;1m=" * 40)
-print(u"\u001b[37;1m||      CPU      ||      MEMORY       ||")
+print(u"\u001b[37;1m||      CPU       ||      MEMORY      ||")
 print(u"\u001b[37;1m=" * 40)
 
 #imprime as informações enquanto o programa rodar
 while True:
     try:
-        print(u"\u001b[37;1m||     "+ get_cpu_usage()+"%      \u001b[37;1m||      "+get_mem_usage()+"%       \u001b[37;1m||")
+        print(u"\u001b[37;1m||     "+ get_cpu_usage()+"%      \u001b[37;1m||      "+get_mem_usage()+"%        \u001b[37;1m||")
         print(u"\u001b[37;1m-"*40)
         time.sleep(1)
+    
     except KeyboardInterrupt:
         print("\nPrograma finalizado!")
         break
